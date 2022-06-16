a27cc40e41967dfa5dbe268eaf4b95c6959e5aa3
GyanPrakashRaj <thecornerstandingman@gmail.com>
2022-06-17 02:26:09 +0530

    hellostranger

--- G_42/scary.py
+++ G_42/scary.py
@@ -17,3 +17,12 @@ def gaussian_logprob(noise, log_std):
     """Compute Gaussian log probability."""
     residual = (-0.5 * noise.pow(2) - log_std).sum(-1, keepdim=True)
     return residual - 0.5 * np.log(2 * np.pi) * noise.size(-1)
+
+
+def squash(mu, pi, log_pi):
+    mu = torch.tanh(mu)
+    if pi is not None:
+        pi = torch.tanh(pi)
+    if log_pi is not None:
+        log_pi -= torch.log(F.relu(1 - pi.pow(2)) + 1e-6).sum(-1, keepdim=True)
+    return mu, pi, log_pi
