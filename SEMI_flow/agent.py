f6d2f6edc1a571c809a8d20d6952246957084835
GyanPrakashRaj <thecornerstandingman@gmail.com>
2022-06-19 23:04:54 +0530

    you deed:fire:

---
+++ SEMI_flow/agent.py
@@ -0,0 +1,74 @@
+from __future__ import division
+import os
+import numpy as np
+import torch
+from torch import optim
+from torch.nn.utils import clip_grad_norm_
+
+from model import DQN
+
+from itertools import chain
+from torch.nn import functional as F
+
+class Agent():
+  def __init__(self, args, env):
+    self.action_space = env.action_space()
+    self.atoms = args.atoms
+    self.Vmin = args.V_min
+    self.Vmax = args.V_max
+    self.support = torch.linspace(args.V_min, args.V_max, self.atoms).to(device=args.device)  # Support (range) of z
+    self.delta_z = (args.V_max - args.V_min) / (self.atoms - 1)
+    self.batch_size = args.batch_size
+    self.n = args.multi_step
+    self.discount = args.discount
+    self.norm_clip = args.norm_clip
+
+    self.online_net = DQN(args, self.action_space).to(device=args.device)
+    if args.model:  # Load pretrained model if provided
+      if os.path.isfile(args.model):
+        state_dict = torch.load(args.model, map_location='cpu')  # Always load tensors onto CPU by default, will shift to GPU if necessary
+        if 'conv1.weight' in state_dict.keys():
+          for old_key, new_key in (('conv1.weight', 'convs.0.weight'), ('conv1.bias', 'convs.0.bias'), ('conv2.weight', 'convs.2.weight'), ('conv2.bias', 'convs.2.bias'), ('conv3.weight', 'convs.4.weight'), ('conv3.bias', 'convs.4.bias')):
+            state_dict[new_key] = state_dict[old_key]  # Re-map state dict for old pretrained models
+            del state_dict[old_key]  # Delete old keys for strict load_state_dict
+        self.online_net.load_state_dict(state_dict)
+        print("Loading pretrained model: " + args.model)
+      else:  # Raise error if incorrect model path provided
+        raise FileNotFoundError(args.model)
+
+    self.online_net.train()
+
+    self.target_net = DQN(args, self.action_space).to(device=args.device)
+    self.update_target_net()
+    self.target_net.train()
+    for param in self.target_net.parameters():
+      param.requires_grad = False
+
+    # self.optimiser = optim.Adam(self.online_net.parameters(), lr=args.learning_rate, eps=args.adam_eps)
+    self.convs_optimiser = optim.Adam(self.online_net.convs.parameters(), lr=args.learning_rate, eps=args.adam_eps)
+    self.linear_optimiser = optim.Adam(chain(self.online_net.fc_h_v.parameters(), self.online_net.fc_h_a.parameters(), 
+                                        self.online_net.fc_z_v.parameters(), self.online_net.fc_z_a.parameters()), lr=args.learning_rate, eps=args.adam_eps)
+
+  # Resets noisy weights in all linear layers (of online net only)
+  def reset_noise(self):
+    self.online_net.reset_noise()
+
+  # Acts based on single state (no batch)
+  def act(self, state):
+    
+    with torch.no_grad():
+      # don't count these calls since it is accounted for after "action = dqn.act(state)" in main.py
+      ret = (self.online_net(state.unsqueeze(0)) * self.support).sum(2).argmax(1).item()
+      return ret
+
+  # Acts with an ε-greedy policy (used for evaluation only)
+  def act_e_greedy(self, state, epsilon=0.001):  # High ε can reduce evaluation scores drastically
+    return np.random.randint(0, self.action_space) if np.random.random() < epsilon else self.act(state)
+
+  def learn(self, mem, freeze=False):
+    # Sample transitions
+    idxs, states, actions, returns, next_states, nonterminals, weights, _ = mem.sample(self.batch_size)
+
+    # Calculate current state probabilities (online network noise already sampled)
+    log_ps = self.online_net(states, log=True)  # Log probabilities log p(s_t, ·; θonline)
+    log_p
