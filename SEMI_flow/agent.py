bc9e7199fbc04459d6d5e611bc69b963b5f4a423
GyanPrakashRaj <thecornerstandingman@gmail.com>
2022-08-28 10:54:58 +0530

    :v:Back Again

--- SEMI_flow/agent.py
+++ SEMI_flow/agent.py
@@ -97,3 +97,12 @@ class Agent():
       offset = torch.linspace(0, ((self.batch_size - 1) * self.atoms), self.batch_size).unsqueeze(1).expand(self.batch_size, self.atoms).to(actions)
       m.view(-1).index_add_(0, (l + offset).view(-1), (pns_a * (u.float() - b)).view(-1))  # m_l = m_l + p(s_t+n, a*)(u - b)
       m.view(-1).index_add_(0, (u + offset).view(-1), (pns_a * (b - l.float())).view(-1))  # m_u = m_u + p(s_t+n, a*)(b - l)
+
+    loss = -torch.sum(m * log_ps_a, 1)  # Cross-entropy loss (minimises DKL(m||p(s_t, a_t)))
+    self.online_net.zero_grad()
+    loss.mean().backward()  # Backpropagate importance-weighted minibatch loss
+    clip_grad_norm_(self.online_net.parameters(), self.norm_clip)  # Clip gradients by L2 norm
+    # self.optimiser.step()
+    if not freeze:
+      self.convs_optimiser.step()
+    self.linear_optimiser.step()
