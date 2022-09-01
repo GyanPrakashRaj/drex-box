1fe87bc03616abdb7370fbbad618d940df34074f
GyanPrakashRaj <thecornerstandingman@gmail.com>
2022-09-01 19:10:25 +0530

    main

---
+++ SEMI_flow/main.py
@@ -0,0 +1,106 @@
+from __future__ import division
+import argparse
+import bz2
+from datetime import datetime
+import os
+import pickle
+
+import atari_py
+import numpy as np
+import torch
+from tqdm import trange
+
+from agent import Agent
+from env import Env
+from memory import ReplayMemory # not used, since we remove PER in Rainbow, but could be added back
+from memory_no_per import ReplayMemory2
+from test import test
+
+from torch import nn
+
+# Note that hyperparameters may originally be reported in ATARI game frames instead of agent steps
+parser = argparse.ArgumentParser(description='Rainbow')
+parser.add_argument('--id', type=str, default='default', help='Experiment ID')
+parser.add_argument('--seed', type=int, default=123, help='Random seed')
+parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
+parser.add_argument('--game', type=str, default='space_invaders', choices=atari_py.list_games(), help='ATARI game')
+parser.add_argument('--T-max', type=int, default=int(50e6), metavar='STEPS', help='Number of training steps (4x number of frames)')
+parser.add_argument('--max-episode-length', type=int, default=int(108e3), metavar='LENGTH', help='Max episode length in game frames (0 to disable)')
+parser.add_argument('--history-length', type=int, default=4, metavar='T', help='Number of consecutive states processed')
+parser.add_argument('--architecture', type=str, default='canonical', choices=['canonical', 'data-efficient'], metavar='ARCH', help='Network architecture')
+parser.add_argument('--hidden-size', type=int, default=512, metavar='SIZE', help='Network hidden size')
+parser.add_argument('--noisy-std', type=float, default=0.1, metavar='σ', help='Initial standard deviation of noisy linear layers')
+parser.add_argument('--atoms', type=int, default=51, metavar='C', help='Discretised size of value distribution')
+parser.add_argument('--V-min', type=float, default=-10, metavar='V', help='Minimum of value distribution support')
+parser.add_argument('--V-max', type=float, default=10, metavar='V', help='Maximum of value distribution support')
+parser.add_argument('--model', type=str, metavar='PARAMS', help='Pretrained model (state dict)')
+parser.add_argument('--memory-capacity', type=int, default=int(1e6), metavar='CAPACITY', help='Experience replay memory capacity')
+parser.add_argument('--replay-frequency', type=int, default=4, metavar='k', help='Frequency of sampling from memory')
+parser.add_argument('--priority-exponent', type=float, default=0.5, metavar='ω', help='Prioritised experience replay exponent (originally denoted α)')
+parser.add_argument('--priority-weight', type=float, default=0.4, metavar='β', help='Initial prioritised experience replay importance sampling weight')
+parser.add_argument('--multi-step', type=int, default=3, metavar='n', help='Number of steps for multi-step return')
+parser.add_argument('--discount', type=float, default=0.99, metavar='γ', help='Discount factor')
+parser.add_argument('--target-update', type=int, default=int(8e3), metavar='τ', help='Number of steps after which to update target network')
+parser.add_argument('--reward-clip', type=int, default=1, metavar='VALUE', help='Reward clipping (0 to disable)')
+parser.add_argument('--learning-rate', type=float, default=0.0000625, metavar='η', help='Learning rate')
+parser.add_argument('--adam-eps', type=float, default=1.5e-4, metavar='ε', help='Adam epsilon')
+parser.add_argument('--batch-size', type=int, default=32, metavar='SIZE', help='Batch size')
+parser.add_argument('--norm-clip', type=float, default=10, metavar='NORM', help='Max L2 norm for gradient clipping')
+parser.add_argument('--learn-start', type=int, default=int(20e3), metavar='STEPS', help='Number of steps before starting training')
+parser.add_argument('--evaluate', action='store_true', help='Evaluate only')
+parser.add_argument('--evaluation-interval', type=int, default=100000, metavar='STEPS', help='Number of training steps between evaluations')
+parser.add_argument('--evaluation-episodes', type=int, default=10, metavar='N', help='Number of evaluation episodes to average over')
+# TODO: Note that DeepMind's evaluation method is running the latest agent for 500K frames ever every 1M steps
+parser.add_argument('--evaluation-size', type=int, default=500, metavar='N', help='Number of transitions to use for validating Q')
+parser.add_argument('--render', action='store_true', help='Display screen (testing only)')
+parser.add_argument('--enable-cudnn', action='store_true', help='Enable cuDNN (faster but nondeterministic)')
+parser.add_argument('--checkpoint-interval', default=0, type=int, help='How often to checkpoint the model, defaults to 0 (never checkpoint)')
+parser.add_argument('--memory', help='Path to save/load the memory from')
+parser.add_argument('--disable-bzip-memory', action='store_true', help='Don\'t zip the memory file. Not recommended (zipping is a bit slower and much, much smaller)')
+
+# number of training steps until freezing
+parser.add_argument('--steps-until-freeze', type=int, default=2000000)
+# increase replay capacity by factor according to size difference between latent and raw image (see buffer_increase_factor)
+parser.add_argument('--increase-buffer', default=False, action='store_true')
+# if loading from checkpoint
+parser.add_argument('--pretrained-steps', type=int, default=0)
+# Setup
+args = parser.parse_args()
+
+print(' ' * 26 + 'Options')
+for k, v in vars(args).items():
+  print(' ' * 26 + k + ': ' + str(v))
+results_dir = os.path.join('results', args.id)
+if not os.path.exists(results_dir):
+  os.makedirs(results_dir)
+metrics = {'steps': [], 'rewards': [], 'Qs': [], 'best_avg_reward': -float('inf')}
+np.random.seed(args.seed)
+torch.manual_seed(np.random.randint(1, 10000))
+if torch.cuda.is_available() and not args.disable_cuda:
+  args.device = torch.device('cuda')
+  torch.cuda.manual_seed(np.random.randint(1, 10000))
+  torch.backends.cudnn.enabled = args.enable_cudnn
+else:
+  args.device = torch.device('cpu')
+
+# Simple ISO 8601 timestamped logger
+def log(s):
+  print('[' + str(datetime.now().strftime('%Y-%m-%dT%H:%M:%S')) + '] ' + s)
+
+
+def load_memory(memory_path, disable_bzip):
+  if disable_bzip:
+    with open(memory_path, 'rb') as pickle_file:
+      return pickle.load(pickle_file)
+  else:
+    with bz2.open(memory_path, 'rb') as zipped_pickle_file:
+      return pickle.load(zipped_pickle_file)
+
+
+def save_memory(memory, memory_path, disable_bzip):
+  if disable_bzip:
+    with open(memory_path, 'wb') as pickle_file:
+      pickle.dump(memory, pickle_file)
+  else:
+    with bz2.open(memory_path, 'wb') as zipped_pickle_file:
+      pickle.dump(memory, zipped_pickle_file)
